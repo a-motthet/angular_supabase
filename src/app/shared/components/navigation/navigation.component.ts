5ca4f5199a1e8fd2648380dbd8d713c390aaa162
a-motthet <Atsadawut.mott@gmail.com>
2025-06-02 13:32:42 +0700

    add responsive deside

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  Router,
+  NavigationEnd,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   isMobileMenuOpen = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.reloadNav();
+      }
+    });
+  }
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  reloadNav() {
+    this.isMobileMenuOpen = false;
   }
 }
